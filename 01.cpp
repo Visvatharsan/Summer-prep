b5444d00d7e759e97eff67cd5eb8d9e9ee7e1731
Visvatharsan <visvatharsan6@gmail.com>
2026-06-22 11:29:59 +0530

    0.1

---
+++ 01.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char name = 'A';
+    bool ans = true;
+
+    //int -> 4 bytes
+    //long -> 8 bytes
+    //float -> 4 bytes
+    //double -> 8 bytes
+
+    //conditional statements
+
+    // if(cond.){
+    //     //if cond is true, this block will be executed
+    // }
+    // else{
+    //     //otherwise this will be executed;
+    // }
+
+    
+
+    // char grade = 'A';
+
+    // switch(grade){
+    //     case 'A' : 
+    //     cout << "Excellent";
+    //     case 'B' : 
+    //     cout << "Gang";
+    //     default:
+    //     cout<<"hi";
+    // }
+
+    int x= 10, y=20;
+    (x>y)?cout<<"x is greater" : cout << "y is greater";
+}
