0277055a9db118156e1f6109285587872bca364b
Visvatharsan <149995659+Visvatharsan@users.noreply.github.com>
2026-06-28 00:20:42 +0530

    Time: 0 ms (100%), Space: 15.6 MB (19.82%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-
-        int m = mat.size();
-        int n = mat[0].size();
-
-        if(m * n != r * c) {
+        vector<int> arr;
+        if((r*c) != mat.size() * mat[0].size() ){
             return mat;
         }
 
-        vector<int> store;
-        vector<vector<int>> arr(r, vector<int>(c));
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                store.push_back(mat[i][j]);
+        for(int i=0; i<mat.size(); i++){
+            for(int j=0; j<mat[i].size(); j++){
+                arr.push_back(mat[i][j]);
             }
         }
 
-        int k = 0;
-        for(int i = 0; i < r; i++) {
-            for(int j = 0; j < c; j++) {
-                arr[i][j] = store[k++];
+        vector<vector<int>> ans(r,vector<int>(c));
+
+        int k=0;
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                ans[i][j]=arr[k++];
             }
         }
-
-        return arr;
+        return ans;
     }
 };
