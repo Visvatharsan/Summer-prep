11cca3ee53705845caf897e9005dd4f0438e2e62
Visvatharsan <149995659+Visvatharsan@users.noreply.github.com>
2026-07-10 11:56:01 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.84%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char i: s){
+            if(i == '(' || i == '[' || i == '{'){
+                st.push(i);
+            }
+            else if(st.empty()){
+                return false;
+            }
+            else if((i == ')' && st.top() == '(') || (i == ']' && st.top() == '[') || (i == '}' && st.top() == '{')){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        if(st.size() > 0){
+            return false;
+        }
+        return true;
+    }
+};
