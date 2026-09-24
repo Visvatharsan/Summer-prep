6e57f85617f8f7f073d1560ca67f28a7463679e4
Visvatharsan <149995659+Visvatharsan@users.noreply.github.com>
2026-09-24 11:10:19 +0530

    Time: 129 ms (95.11%), Space: 20.6 MB (44.77%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.py
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.py
@@ -1,4 +1,4 @@
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
         temp = sorted(nums)
-        return ((temp[-1]*temp[-2])-(temp[0]*temp[1]))
+        return abs((temp[0]*temp[1])-(temp[-1]*temp[-2]))
